10fea5dfa274536741c263b6bb82abb45808dd92
Viktor Rozenko <sharp.vik@gmail.com>
2018-05-24 00:42:39 -0400

    synt DONE

--- lib/synt.py
+++ lib/synt.py
@@ -1,14 +1,6 @@
     # imports
-from lib import prefuncs as func
+import prefuncs as func
 from inspect import signature
-
-
-
-
-
-    # arguments count for a function
-def argc(function):
-    return len( signature(function).parameters )
 
 
 
@@ -19,12 +11,6 @@ def synt(postfix):
         # variables
     var = func.Stack()
     exp = list(postfix)
-
-
-    # operators
-    ops = [ '+', '-', '*', '/', '//',
-            '%', '^', '!', '$',
-            '(', ')' ]
 
 
         # operators
@@ -43,12 +29,19 @@ def synt(postfix):
         '$'     : ( lambda x: func.prime_check(x) )
     }
 
+
         # execution
     for tok in postfix:
         if tok not in ops:      # for numbers
-            var.push(tok)
+            var.push( int(tok) )
         else:                   # for operators
-            pass
+            if tok == '!' or tok == '$':        # for toks with 1 arg
+                arg = var.pop()
+                var.push( ops[tok](arg) )
+            else:                               # for toks with 2 args
+                arg2 = var.pop()
+                arg1 = var.pop()
+                var.push( ops[tok](arg1, arg2) )
 
 
         # output
