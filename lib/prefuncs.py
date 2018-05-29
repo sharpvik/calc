5f9b40fab8b45adb5d5500adc695dd86bf885357
Viktor Rozenko <sharp.vik@gmail.com>
2018-05-29 17:09:06 -0400

    error checking

--- lib/prefuncs.py
+++ lib/prefuncs.py
@@ -6,23 +6,34 @@ from math import sqrt
 
 
 def prime_check(n):                     # n(int) -- any positive number; --> function returns True or False
-    a = 2
-    if n == a: return True
-    elif n < a: return False
+    if not isinstance(n, int):
+        raise ValueError("prime_check() arg must be of type int")
+    elif n < 0:
+        raise ValueError("prime_check() arg must be a positive number")
     else:
-        while a <= int( sqrt(n) ) + 1:
-            if n % a == 0: return False
-            else: a += 1
+        a = 2
+        if n == a: return True
+        elif n < a: return False
         else:
-            return True
+            while a <= int( sqrt(n) ) + 1:
+                if n % a == 0: return False
+                else: a += 1
+            else:
+                return True
 
 
 def factorial(n):   # n(int) -- any positive number; --> function returns n!;
-    fact = n
-    if n > 2: 
-        for i in range(n - 1, 0, -1):
-            fact *= i
-    return fact
+    if not isinstance(n, int):
+        raise ValueError("factorial() arg must be of type int")
+    elif n < 0:
+        raise ValueError("factorial() arg must be a positive number")
+    elif n < 2:
+        return 1
+    else:
+        f = 1
+        for i in range(1, n + 1):
+            f *= i
+        return f
 
 
 def nPr(n, r):      # n(int) -- number of items; r(int) -- number of positions; --> function returns number of permutations of n elements for r positions;
